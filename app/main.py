803a9c04bb9764715bfb6df5a342ba01735420fd
Josh <josh@civo.com>
2024-04-14 12:31:25 -0400

    adding example

--- app/main.py
+++ app/main.py
@@ -1,9 +1,46 @@
 from flask import Flask
+from openai import OpenAI
+from os import path
 app = Flask(__name__)
 
-@app.route('/api')
+#  check if the application is running in a container
+is_container = (
+    path.exists("/var/run/secrets/kubernetes.io/serviceaccount/namespace")
+    or path.exists("/.dockerenv")
+    or (
+        path.isfile("/proc/self/cgroup")
+        and (
+            any("kubepods" in line for line in open("/proc/self/cgroup"))
+            or any("docker" in line for line in open("/proc/self/cgroup"))
+        )
+    )
+)
+
+@app.route('/')
 def return_results():
-    return 'Hello World!'
+    
+    # use the localhost if not running in a container
+    if is_container:
+        ollama_api_endpoint = "http://ollama.ollama.svc.cluster.local:11434"
+    else:
+        ollama_api_endpoint = "http://localhost:11434" # Development endpoint port forwarded
+    
+    client = OpenAI(
+        base_url = ollama_api_endpoint + '/v1',
+        api_key='forced_key', # required, but unused
+    )
+    
+    # generate a response
+    response = client.chat.completions.create(
+        model="gemma:latest",
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": "Write a motivational quote."},
+        ]
+    )
+    
+    # return the response
+    return response.choices[0].message.content
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
