fa696a983917ba12691f253feaec656e6ee17fb3
Josh <josh@civo.com>
2024-04-24 10:45:23 +0100

    updated to use L40s and Llama3

--- infra/tf/variables.tf
+++ infra/tf/variables.tf
@@ -7,15 +7,15 @@ variable "cluster_name" {
     type    = string
     default = "llm_boilerplate"
     description = "The name of the cluster to create"
-
 }
 
 # the GPU node instance to use for the cluster
 variable "cluster_node_size" {
     type    = string
-    default = "g4g.40.kube.small"
+    # default = "g4g.40.kube.small"  # A100 40gb
+    # default = "g4g.kube.small" # A100 80gb
+    default = "an.g1.l40s.x1"  # L40s 46gb
     description = "The size of the GPU node required for the cluster"
-
 }
 
 # the number of nodes to provision in the cluster
