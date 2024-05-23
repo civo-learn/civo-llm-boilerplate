d9ff06df79fd7e9923c7d78ab9e7154a081d3495
Josh <josh@civo.com>
2024-05-23 15:14:52 +0100

    removing gemma from the initial deployment and replacing with llama3

--- app/main.py
+++ app/main.py
@@ -32,7 +32,7 @@ def return_results():
     
     # generate a response
     response = client.chat.completions.create(
-        model="gemma:latest",
+        model="llama3:latest",
         messages=[
             {"role": "system", "content": "You are a helpful assistant."},
             {"role": "user", "content": "Write a motivational quote."},
