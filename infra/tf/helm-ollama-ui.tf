5e944e3d700c26b6252554ba571ab7dffb91a897
Ben Norris <bennorris@Bens-MacBook-Pro.local>
2024-11-10 18:18:50 +0000

    Readd timeout param

--- infra/tf/helm-ollama-ui.tf
+++ infra/tf/helm-ollama-ui.tf
@@ -15,14 +15,15 @@ resource "helm_release" "ollama-ui" {
 
   set {
     name = "webui.image.tag"
-    value = var.ollama_ui_image_version
+    value = var.openwebui_image_tag
   }
 
-  #timeout = 900 # 15 minutes in seconds.
+  timeout = 900 # 15 minutes in seconds.
 }
 
 
 data "kubernetes_service" "ollama-ui" {
+  count = var.deploy_ollama_ui ? 1 : 0
   metadata {
     name      = "open-webui"
     namespace = "ollama-ui"
