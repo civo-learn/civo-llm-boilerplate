fdb3a5ecd0f9a799a11a393e313ff404dfd9e29a
Subhasmita <subhasmitaswain232@gmail.com>
2024-12-13 12:29:06 +0530

    fix terraform misconfig

--- infra/tf/helm-app.tf
+++ infra/tf/helm-app.tf
@@ -9,11 +9,16 @@ resource "helm_release" "app" {
 depends_on = [ local_file.cluster-config ]
 }
 
+resource "time_sleep" "wait_for_app" {
+  depends_on = [helm_release.app]
+  create_duration = "30s"
+}
+
 data "kubernetes_service" "app" {
   metadata {
     name      = "app"
     namespace = "apps"
   }
 
-  depends_on = [helm_release.app]
+  depends_on = [time_sleep.wait_for_app]
 }
--- infra/tf/variables.tf
+++ infra/tf/variables.tf
@@ -87,10 +87,10 @@ variable "ollama_ui_image_version" {
 
 # Output the ollama-ui service IP
 output "ollama_ui_service_ip" {
-  value = var.deploy_ollama_ui ? data.kubernetes_service.ollama-ui[0].status.0.load_balancer.0.ingress.0.ip : null
+  value = var.deploy_ollama_ui ? data.kubernetes_service.ollama-ui.status.0.load_balancer.0.ingress.0.ip : null
 }
 
 # Output the web app load balancer public IP
 output "ollama_app_load_balancer_ip" {
-  value = var.deploy_app ? data.kubernetes_service.app[0].status.0.load_balancer.0.ingress.0.ip : null 
+  value = var.deploy_app ? data.kubernetes_service.app.status.0.load_balancer.0.ingress.0.ip : null
 }
